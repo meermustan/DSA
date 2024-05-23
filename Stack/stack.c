d0bda5b51f2cd53c8ba06c5db4775e4dd49ec6ba
meermustan <meer.mustan786@gmail.com>
2024-05-23 20:11:30 +0500

    Implemented the stack basic code in C

--- Stack/stack.c
+++ Stack/stack.c
@@ -1,8 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-    
+struct stack {
+    int size;
+    int top;
+    int * arr;
 
+};
+
+int isEmpty(struct stack* ptr){
+    if(ptr->top == -1){
+        printf("Stack is empty\n");
+        return 1;
+    }else{
+        return 0;
+    }
+};
+
+int isFull(struct stack* ptr){
+    if(ptr->size >= ptr->top + 1){
+        printf("Stack is full\n");
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+int main(){
+     struct stack *s = (struct stack *)malloc(sizeof(struct stack));
+    // struct stack s;
+    s->size = 80;
+    s->top = -1;
+    s->arr = (int *)malloc(s->size * sizeof(int));
+
+    isEmpty(s);
     return 1;
 };
