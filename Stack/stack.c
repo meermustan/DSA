47158b56bcb20a5d8aaf588ddda07af2eeced78b
meermustan <meer.mustan786@gmail.com>
2024-05-25 20:12:40 +0500

    Learned and Implemented the stack and Stack with Linked list in C

--- Stack/stack.c
+++ Stack/stack.c
@@ -10,7 +10,6 @@ struct stack {
 
 int isEmpty(struct stack* ptr){
     if(ptr->top == -1){
-        printf("Stack is empty\n");
         return 1;
     }else{
         return 0;
@@ -18,7 +17,7 @@ int isEmpty(struct stack* ptr){
 };
 
 int isFull(struct stack* ptr){
-    if(ptr->size >= ptr->top + 1){
+    if(ptr->top == ptr->size - 1){
         printf("Stack is full\n");
         return 1;
     }else{
@@ -26,13 +25,87 @@ int isFull(struct stack* ptr){
     }
 }
 
+void push(struct stack * ptr, int data){
+    if(isFull(ptr)){
+        printf("Stack is full\n");
+    }else{
+        ptr->top++;
+        ptr->arr[ptr->top] = data;
+        return 1;
+    }
+}
+
+int pop(struct stack * ptr){
+    if(isEmpty(ptr)){
+        printf("Stack is empty\n");
+    }else{
+        int value = ptr->arr[ptr->top];
+        ptr->top--;
+        ptr->arr[ptr->top + 1] = 0; // free the value by providing zero value
+        return value;
+    }
+}
+
+void traversal(struct stack * ptr){
+    if(isEmpty(ptr)){
+        printf("Stack is empty\n");
+        return 0;
+    }
+    int i = 0;
+    while(i <= ptr->top){
+        printf("%d  ", ptr->arr[i]);
+        i++;
+    }
+    printf("\n");
+    return 1;
+}
+
+int peek(struct stack * ptr, int i){
+    int arrayIndex = ptr->top -i + 1;
+    if(arrayIndex < 0){
+        printf("Not valid position in stack\n");
+    }else{
+        return ptr->arr[arrayIndex];
+    }
+}
+
+int stackTop(struct stack * ptr){
+    if(ptr->top >= 0){
+        return ptr->arr[ptr->top];
+    }
+}
+
+int stackBottom(struct stack * ptr){
+    if(ptr->top >= 0){
+        return ptr->arr[0];
+    }
+}
+
 int main(){
-     struct stack *s = (struct stack *)malloc(sizeof(struct stack));
+    struct stack *s = (struct stack *)malloc(sizeof(struct stack));
     // struct stack s;
     s->size = 80;
     s->top = -1;
     s->arr = (int *)malloc(s->size * sizeof(int));
 
-    isEmpty(s);
+    push(s, 30);
+    push(s, 80);
+    push(s, 28);
+
+    
+    
+    for (int j = 1; j <= (s->top + 1) ; j++)
+    {
+        printf("The value at position %d is %d \n", j,peek(s, j));
+    }
+
+    printf("The stack top is %d \n", stackTop(s));
+    printf("The stack bottom is %d \n", stackBottom(s));
+    
+
+    if(isEmpty(s)){
+        printf("Stack is empty \n");
+    }
+
     return 1;
 };
---
+++ StackLL/stackLL.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node{
+    int data;
+    struct Node * next;
+};
+
+struct Node * top = NULL;
+
+void linkListTraversal(struct Node * ptr){
+    while (ptr != NULL)
+    {
+        printf("%d \n", ptr->data);
+        ptr = ptr->next;
+    }
+    return 1;
+};
+
+int isEmpty(struct Node * top){
+    if(top == NULL){
+        return 1;
+    }else{
+        return 0;
+    }
+};
+
+int isFull(struct Node * top){
+    struct Node * p = (struct Node *)malloc(sizeof(struct Node));
+    if(p == NULL){
+        return 1;
+    }else{
+        return 0;
+    }
+};
+
+struct Node * push(struct Node * top, int x){
+    if(isFull(top)){
+        printf("Stack is full.\n");
+    }else{
+        struct Node * n = (struct Node *)malloc(sizeof(struct Node));
+        n->data = x;
+        n->next = top;
+        top = n;
+        return top;
+    }
+};
+
+int peek(int pos){
+    struct Node * ptr = top;
+    for (int i = 0;(i < pos -1 && ptr != NULL); i++)
+    {
+        ptr = ptr->next;
+    }
+    if(ptr != NULL){
+        return ptr->data;
+    }else{
+        return -1;
+    }
+    
+}
+
+int pop(struct Node * tp){
+    if(isEmpty(tp)){
+        printf("Stack is empty.\n");
+    }else{
+        struct Node * n = tp;
+        top = tp->next;
+        int x = n->data;
+        free(n);
+        return x;
+    }
+};
+
+int main(){
+    top = push(top, 38);
+    top = push(top, 29);
+    top = push(top, 68);
+    linkListTraversal(top);
+    printf("The popped value is %d\n", pop(top));
+    linkListTraversal(top);
+    printf("The peek value is %d\n", peek(1));
+    
+    return 1;
+};
